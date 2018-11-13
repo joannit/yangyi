7df299562b08e32a571d23c864ec65825b7c11d1
joannit <joannitdream.@163.com>
2018-11-13 18:52:50 +0800

    add 后台权限，用户，,会员

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/user',function() {
-    echo  'this is test page';
+
+// 后台登录和退出
+Route::resource('/adminlogin','Admin\AdminLoginController');
+
+// 登录全向管理
+Route::group(['middleware'=>'adminlogin'],function(){
+
+    // 后台首页
+    Route::get("/admin",'Admin\AdminController@index');
+
+
+    //前台用户(会员)管理路由
+    Route::resource ("/admin/user","Admin\UserController");
+
+
+    // 后台用户管理
+    Route::resource("/admin/master",'Admin\MasterController');
+    // 分配权限
+
+    Route::resource("/adminpower","Admin\PowerController");
+
+    // 后台用户密码修改
+    Route::get('/admineditpwd','Admin\MasterController@admineditpwd');
+    // 执行密码修改
+    Route::post('/adminupdatepwd','Admin\MasterController@adminupdatepwd');
+    // 后台用户手机号修改
+    Route::get('/admineditpho','Admin\MasterController@admineditpho');
+    // 执行号码修改
+    Route::post('/adminupdatepho','Admin\MasterController@adminupdatepho');
+
 });
+
