f2fc78221144c6845088bc2c84e6e8b01b13f8f1
unknown <1751190143@qq.com>
2018-11-14 21:03:45 +0800

    pz edit web

--- routes/web.php
+++ routes/web.php
@@ -63,3 +63,21 @@ Route::group(['middleware'=>'adminlogin'],function(){
  //后台公告ajax删除
     Route::get('/noticedel','Admin\NoticeController@del');
 
+//前台
+Route::resource("/homeregister","Home\HomeRegisterController");
+//前台注册
+Route::get("/regits","Home\HomeRegisterController@regits");
+//短信验证
+Route::get("/phone","Home\HomeRegisterController@phone");
+//发送验证
+Route::get("/code","Home\HomeRegisterController@code");
+//校验手机
+Route::get("/dophone","Home\HomeRegisterController@dophone");
+//注册成功去登录
+Route::post("/doregister","Home\HomeRegisterController@doregister");
+//角色列表ajax状态修改
+Route::get("/ajaxedit","Admin\RolelistController@ajax");
+//用户列表状态修改
+Route::get("/ajaxuser","Admin\AdminuserController@ajaxuser");
+//前台登录
+Route::resource("/homelogin","Home\HomeLoginController");
